a57aac998b8d27647bd794684f89a5750c4c3bc5
Sudhakar Rawat <sudhakarrawat8175@gamil.com>
2021-05-03 21:50:52 +0530

    use of environment variable

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //jshint esversion:6
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -16,7 +17,7 @@ const userSchema = new mongoose.Schema({
    email:String,
    password:String });
 
-const secret = "thisismylittlesecrets.";
+const secret = process.env.SECRET;
 userSchema.plugin(encryption,{secret:secret,encryptedFields:["password"]});
 
 
